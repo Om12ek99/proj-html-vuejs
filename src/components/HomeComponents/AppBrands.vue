b3f785261c5b8ba855c46d7aa953e916c60b69fe
Om12ek99 <omar.elkamili@gmail.com>
2024-05-14 20:14:35 +0200

    add: brands logo

--- src/components/HomeComponents/AppBrands.vue
+++ src/components/HomeComponents/AppBrands.vue
@@ -2,10 +2,59 @@
 export default {
     data(){
         return {
-            
+            brandImages: [
+                {
+                    src:"src/assets/img/brands/abstract.webp",
+                    title:"abstract"
+                },
+                {
+                    src:"src/assets/img/brands/cglobal.webp",
+                    title:"cglobal"
+                },
+                {
+                    src:"src/assets/img/brands/digitalbox.webp",
+                    title:"digitalbox"
+                },
+                {
+                    src:"src/assets/img/brands/hemisferio.webp",
+                    title:"hemisferio"
+                },
+                {
+                    src:"src/assets/img/brands/next.webp",
+                    title:"next"
+                },
+                {
+                    src:"src/assets/img/brands/spaces.webp",
+                    title:"spaces"
+                }
+            ]
         }
     }
 }
 </script>
-<template></template>
-<style scoped lang="scss"></style>
+<template>
+    <div class="container">
+        <div class="row">
+            <h1>Some Brands We Work With</h1>
+            <h3>Quisque aliquet, libero consequat elementum convallis.</h3>
+        </div>
+        <div class="row">
+            <div class="col" v-for="brand in brandImages">
+                <img :src="brand.src" alt="">
+            </div>
+
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+.container{
+    .row:first-child{
+       text-align: center;
+       margin-top:2rem;
+       margin-bottom: 2rem;
+    }
+}
+img{
+    width: 150px;
+}
+</style>
--- src/components/HomeComponents/AppConsultationCards.vue
+++ src/components/HomeComponents/AppConsultationCards.vue
@@ -1,28 +1,10 @@
 <script>
 export default {
-    // props: {            
-    //         ConsultImage: String,              // immagine di display
-    //         ConsultIntest: String,             // testo della intestazione
-    //         ConsultLogo: String,               // logo della intestazione
-    //         ConsultTitle: String,              // titolo della card
-    //         ConsultText: String,               // testo della card
-    //         ConsultButton: String,             // testo del button            
-    //     },
 };
 </script>
 <template>
     <div class="card">
         <div class="row">
-            <div class="col">
-                <img :src="ConsultImage" alt="display image">
-            </div>
-            <!-- <div class="col">
-                <img :src="ConsultLogo" alt="">
-                <h4>{{ ConsultIntest }}</h4>
-                <h2>{{ ConsultTitle }}</h2>
-                <p>{{ ConsultText }}</p>
-                <button>{{ ConsultButton }}</button>
-            </div> -->
         </div>
     </div>
 </template>
