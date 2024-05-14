fc78120cac5385f146fe181988acd8d8c467ac20
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-14 12:45:40 +0200

    Fix: add hover to footer list

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,85 +1,89 @@
 <script>
 export default {
-    
-    data(){
-        return {
-        
-        }
+
+  data() {
+    return {
 
     }
+
+  }
 }
 
 </script>
 <template>
-    <!-- FOOTER -->
-<footer class="site-footer py-5">
-  <div class="container py-5">
-    <div class="row py-5">
-      <div class="col-lg-3">
-        <!-- TITOLO -->
-        <h3>Grow Your Online Business Strategically, and Improve Customer Retention</h3>
- <!-- LISTA CHE RIUNISCE LE ICONE  -->
-        <ul class="list-unstyled social-icon mb-0">
-          <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-          <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-          <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
-        </ul>
-      </div>
-<!-- GRUPPO LINK A LISTE  -->
-      <div class="col-lg-3 mb-5 mb-lg-0">
-<!-- LISTA SERVIZI -->
-        <h5>Services</h5>
-        <ul class="list-unstyled quick-links">
-          <li><a href="#">Marketing Plan</a></li>
-          <li><a href="#">Sales Development</a></li>
-          <li><a href="#">Digital Marketing</a></li>
-          <li><a href="#">Pricing</a></li>
-          <li><a href="#">Why Us</a></li>
-        </ul>
-      </div>
-<!-- LISTA RISORSE -->
-      <div class="col-lg-3 mb-5 mb-lg-0">
-        <h5>Resources</h5>
-        <ul class="list-unstyled quick-links">
-          <li><a href="#">Learning center</a></li>
-          <li><a href="#">Video Tutorial</a></li>
-          <li><a href="#">Customer</a></li>
-          <li><a href="#">Blog</a></li>
-        </ul>
-      </div>
-<!-- LISTA COMPANY -->
-      <div class="col-lg-3 mb-5 mb-lg-0">
-        <h5>Company</h5>
-        <ul class="list-unstyled quick-links">
-          <li><a href="#">Who We Are</a></li>
-          <li><a href="#">Contact us</a></li>
-          <li><a href="#">Careers</a></li>
-        </ul>
+  <!-- FOOTER -->
+  <footer class="site-footer py-5">
+    <div class="container py-5">
+      <div class="row py-5">
+        <div class="col-lg-3">
+          <!-- TITOLO -->
+          <h3>Grow Your Online Business Strategically, and Improve Customer Retention</h3>
+          <!-- LISTA CHE RIUNISCE LE ICONE  -->
+          <ul class="list-unstyled social-icon mb-0">
+            <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+          </ul>
+        </div>
+        <!-- GRUPPO LINK A LISTE  -->
+        <div class="col-lg-3 mb-5 mb-lg-0">
+          <!-- LISTA SERVIZI -->
+          <h5>Services</h5>
+          <ul class="list-unstyled quick-links">
+            <li><a href="#">Marketing Plan</a></li>
+            <li><a href="#">Sales Development</a></li>
+            <li><a href="#">Digital Marketing</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Why Us</a></li>
+          </ul>
+        </div>
+        <!-- LISTA RISORSE -->
+        <div class="col-lg-3 mb-5 mb-lg-0">
+          <h5>Resources</h5>
+          <ul class="list-unstyled quick-links">
+            <li><a href="#">Learning center</a></li>
+            <li><a href="#">Video Tutorial</a></li>
+            <li><a href="#">Customer</a></li>
+            <li><a href="#">Blog</a></li>
+          </ul>
+        </div>
+        <!-- LISTA COMPANY -->
+        <div class="col-lg-3 mb-5 mb-lg-0">
+          <h5>Company</h5>
+          <ul class="list-unstyled quick-links">
+            <li><a href="#">Who We Are</a></li>
+            <li><a href="#">Contact us</a></li>
+            <li><a href="#">Careers</a></li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
-<!-- FOOTER CREDITS NUMERO MAIL CREATED  -->
-  <div class="footer-copyright">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4 mt-5 text-center">
-          <p>© 2012 -2020 • Avada Consultant • Powered by Team 11 boolean.</p>
-        </div>
+    <!-- FOOTER CREDITS NUMERO MAIL CREATED  -->
+    <div class="footer-copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 mt-5 text-center">
+            <p>© 2012 -2020 • Avada Consultant • Powered by Team 11 boolean.</p>
+          </div>
 
-        <div class="col-md-4 mt-5 text-center">
-          <p>Call Us (555) 802-1234</p>
-        </div>
+          <div class="col-md-4 mt-5 text-center">
+            <p>Call Us (555) 802-1234</p>
+          </div>
 
-        <div class="col-md-4 mt-5 text-center">
-          <p>info@yourcompany.com</p>
-        </div>
+          <div class="col-md-4 mt-5 text-center">
+            <p>info@yourcompany.com</p>
+          </div>
 
+        </div>
       </div>
     </div>
-  </div>
 
-</footer>
+  </footer>
 </template>
 <style scoped lang="scss">
+a:hover {
+
+  color: #f86011;
+}
 </style>
