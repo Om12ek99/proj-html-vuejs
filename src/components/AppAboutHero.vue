431453bd09076e5a31c1220fc360ff0ebac5c7a4
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-14 13:51:00 +0200

    Feat: add AboutHero components

--- src/components/AppAboutHero.vue
+++ src/components/AppAboutHero.vue
@@ -1,7 +1,44 @@
 <script>
-
 </script>
 
 <template>
-     
+    <!--About Hero  -->
+    <section class="d-flex justify-content-center py-5 mt-5">        
+        <div class="py-5 ms_hero text-center w-50">
+            <div class="py-1 ms_hero-title ">
+                We'are a Team of Marketihg Consultants,
+                Passionate in Helping Your Business Grow    
+            </div>
+            <p class="fs-4 py-4"> 
+                Maxime corporis harum pariatur molestiae beatae 
+                ,sequi, laboriosam obcaecati voluptate delectus 
+                rerum totam quis?
+            </p>
+            <a href="#">
+              <button type="button" class="btn ms_button text-light py-2 px-4 fs-3" >
+                Get a Consultation
+              </button>
+            </a>
+        </div>  
+    </section>
+    <!--/About Hero  -->
+
+    <!-- Section img Bg -->
+    <div class="container">
+
+    </div>
+    <!-- /Section img Bg -->
 </template>
+
+
+
+<style scoped lang="scss">
+@use "../style/partial/variables" as *;
+    .ms_hero-title {
+        font-size: 5rem;
+    }
+
+    .ms_button {
+      background-color:$color-orange;
+    }
+</style>
--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -2,35 +2,16 @@
 import AppAboutHero from "../components/AppAboutHero.vue";
  
 export default {
-    componets: {
-      AppAboutHero,
-    },
+    components: {
+      AppAboutHero
+    }
 }
 </script>
 
 
 <template>
-   <AppAboutHero />
-   <section class="d-flex justify-content-center py-5 mt-5">
-        
-        <div class="py-5 ms_hero text-center w-50">
-            <div class="py-1 ms_hero-title ">
-                We'are a Team of Marketihg Consultants,
-                Passionate in Helping Your Business Grow    
-            </div>
-            <p class="fs-4"> 
-                Maxime corporis harum pariatur molestiae beatae 
-                ,sequi, laboriosam obcaecati voluptate delectus 
-                rerum totam quis?
-            </p>
-
-        </div>  
-    </section>
+  <!-- About Section Hero -->
+  <AppAboutHero />
+  <!-- /About Section Hero -->
 </template>
-
-<style scoped lang="scss">
-    .ms_hero-title {
-        font-size: 5rem;
-    }
-</style>
-
+  
