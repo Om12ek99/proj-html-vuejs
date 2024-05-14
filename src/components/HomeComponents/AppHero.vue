4b468d8d97acd40ba30c733301286a5531c49ca1
Om12ek99 <omar.elkamili@gmail.com>
2024-05-14 16:34:30 +0200

    add: container to Apphero

--- src/components/HomeComponents/AppHero.vue
+++ src/components/HomeComponents/AppHero.vue
@@ -15,14 +15,19 @@ export default {
 }
 </script>
 <template>
-    <div class="col">
+    <div class="container py-5">
+        <div class="row col">
         <h1>Unlock Your Online Growth Potential</h1>
         <h3>Online Marketingto secure customer retention, leads, and sales. We focus on the bigger picture.</h3>
-        <Button>Our Services</Button>               //NB ricordarti di mettere l'icona della freccia!
+        <Button>Our Services</Button>               
+        <!-- //NB ricordarti di mettere l'icona della freccia! -->
     </div>
-    <div class="col">
-        <AppForm/>                                  //Qui ci va il form che Valerio fa
+    <div class="row col">
+        <AppForm/>                   
+                       <!-- //Qui ci va il form che Valerio fa -->
     </div>
+    </div>
+   
 </template>
 <style scoped lang="scss">
 </style>
